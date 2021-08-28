28e3449a9358d1321634e6fa12d37df3a1bc6c44
isoppp <prog@d2deck.com>
2021-08-28 13:39:00 +0900

    s3: Serving HTML Templates

--- section3/main.go
+++ section3/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 const port = ":5555"
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
 
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, err1 := template.ParseFiles("./section3/templates/" + tmpl)
+
+	if err1 != nil {
+		fmt.Println("Error in ParseFiles : ", err1)
+		return
+	}
+
+	err2 := parsedTemplate.Execute(w, nil)
+
+	if err2 != nil {
+		fmt.Println("Error parsing template : ", err2)
+		return
+	}
 }
 
 func main() {
@@ -20,4 +38,5 @@ func main() {
 
 	fmt.Printf("Server is running on port %s\n", port)
 	_ = http.ListenAndServe(port, nil)
+
 }
