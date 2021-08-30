e024699cb690287fb72a6430eb0b108c341545f8
isoppp <prog@d2deck.com>
2021-08-30 18:00:14 +0900

    s8: 72. Writing tests for our GET handlers

--- bookings/internal/render/render.go
+++ bookings/internal/render/render.go
@@ -17,6 +17,7 @@ import (
 var functions = template.FuncMap{}
 
 var app *config.AppConfig
+var templatePath = "./bookings/templates"
 
 // NewTemplates sets the config for the template package
 func NewTemplates(a *config.AppConfig) {
@@ -65,7 +66,7 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 
 	myCache := map[string]*template.Template{}
 
-	pages, err := filepath.Glob("./bookings/templates/*.page.tmpl")
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.page.tmpl", templatePath))
 	if err != nil {
 		return myCache, err
 	}
@@ -77,13 +78,13 @@ func CreateTemplateCache() (map[string]*template.Template, error) {
 			return myCache, err
 		}
 
-		matches, err := filepath.Glob("./bookings/templates/*.layout.tmpl")
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.layout.tmpl", templatePath))
 		if err != nil {
 			return myCache, err
 		}
 
 		if len(matches) > 0 {
-			ts, err = ts.ParseGlob("./bookings/templates/*.layout.tmpl")
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.layout.tmpl", templatePath))
 			if err != nil {
 				return myCache, err
 			}
