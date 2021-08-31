79ad40974fcc0aa4576149024e9f45a010233ac9
isoppp <prog@d2deck.com>
2021-08-31 17:53:48 +0900

    s8: 78. Solution to writing tests for the Forms package

--- bookings/internal/forms/forms.go
+++ bookings/internal/forms/forms.go
@@ -2,29 +2,32 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
 	"github.com/asaskevich/govalidator"
 )
 
+// Form creates a custom form struct and embeds a url.Values object
 type Form struct {
 	url.Values
-	Errors errors //nolint:typecheck
+	Errors errors
 }
 
-func New(data url.Values) *Form {
-	return &Form{
-		data,
-		errors(map[string][]string{}), //nolint:typecheck
-	}
-}
-
+// Valid returns true if there are no errors, otherwise false
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
 }
 
+// New initializes a form struct
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+// Required checks for required fields
 func (f *Form) Required(fields ...string) {
 	for _, field := range fields {
 		value := f.Get(field)
@@ -34,16 +37,18 @@ func (f *Form) Required(fields ...string) {
 	}
 }
 
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+// Has checks if form field is in post and not empty
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
 	if x == "" {
 		return false
 	}
 	return true
 }
 
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+// MinLength check for minimum length
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
@@ -51,6 +56,7 @@ func (f *Form) MinLength(field string, length int, r *http.Request) bool {
 	return true
 }
 
+// IsEmail checks for a valid email address
 func (f *Form) IsEmail(field string) {
 	if !govalidator.IsEmail(f.Get(field)) {
 		f.Errors.Add(field, "Invalid email address")
