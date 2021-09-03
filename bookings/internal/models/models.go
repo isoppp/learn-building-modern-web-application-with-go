8eff498fa6812e95129ce311043a54e29ed34ab5
isoppp <prog@d2deck.com>
2021-09-03 13:40:22 +0900

    s8: 99. Setting up models

--- bookings/internal/models/models.go
+++ bookings/internal/models/models.go
@@ -1,8 +1,63 @@
 package models
 
+import "time"
+
 type Reservation struct {
 	FirstName string
 	LastName  string
 	Email     string
 	Phone     string
+}
+
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomId    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomId        int
+	ReservationId int
+	RestrictionId int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
