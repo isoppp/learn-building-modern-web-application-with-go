eb0c27978583e54c3807de58b170f24d11e4665a
isoppp <prog@d2deck.com>
2021-09-05 01:21:45 +0900

    s8: 115, 116

---
+++ bookings/migrations/20210904161303_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into public.rooms (room_name, created_at, updated_at) VALUES
+('General''s Quarters', '2021-09-01 00:00:00.000', '2021-09-01 00:00:00.000'),
+('Major''s Suite', '2021-09-01 00:00:00.000', '2021-09-01 00:00:00.000');
---
+++ bookings/migrations/20210904161641_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+insert into public.restrictions(restriction_name, created_at, updated_at) values
+('Reservation', '2021-09-04 13:58:57.000000','2021-09-04 13:58:59.000000'),
+('Owner Block' ,'2021-09-06 14:25:49.000000','2021-09-09 14:25:53.000000');
